31b5bb9d70cda9df650e191e87de76d6617c3f20
bolkaaa <kobolka.matus@gmail.com>
2018-05-15 11:01:02 +0000

    new bash script for looping the playlist

--- player/run.sh
+++ player/run.sh
@@ -1,34 +1,57 @@
 #! /bin/bash
-setterm -cursor off
+setterm -term linux -background black -foreground black -cursor off
+
+cleanup ()
+{
+setterm -term linux -background black -foreground white -cursor on	
+kill -s SIGTERM $!
+exit 0
+}
+
+trap cleanup SIGINT SIGTERM
 
 MEDIA_FOLDER="/home/pi/pi_tv/app/static/media"
 PLAYLIST_FILE="${MEDIA_FOLDER}/playlist"
+VIDEO_RECOVER=3
 
-# infinite
-while true; do
+while true; 
+do
 	if [ -e ${PLAYLIST_FILE} ]; then
-		# loop playlist items
 		cat ${PLAYLIST_FILE} | while read LINE
-		do
+		do {
 			FILENAME=$(echo $LINE | cut -d'@' -f1)
 			TYPE=$(echo $LINE | cut -d'@' -f2)
 			DURATION=$(echo $LINE | cut -d'@' -f3)
-			# make tty window nice & clean
 			clear		
 			DEST="${MEDIA_FOLDER}/uploads/${FILENAME}"
-			# lets check if file exists
-			if [ -e ${DEST} ]; then
-				# lets run fbi if it is an image file...
-				if [ "$TYPE" = "image" ]; then	
-					fbi -a -t ${DURATION} -1 ${DEST} # >/dev/null 2>>/dev/null
-					# else
-					# vlc --play-and-exit -q $f	
+			if [[ ( -e ${DEST} ) || ( "$TYPE" = "url" ) ]]; then
+				if [ "$TYPE" = "image" ]
+				then	
+					fbi -d /dev/fb0 -T 1 --noverbose --nocomments -a -t $DURATION -1 ${DEST} > /dev/null 2>>/dev/null	
+				elif [ "$TYPE" = "video" ]
+				then	
+					omxplayer -o hdmi -r ${DEST} > /dev/null 2>>/dev/null
+					sleep $VIDEO_RECOVER
+				elif [ "$TYPE" = "url" ]
+				then	
+					VURL=$(youtube-dl -g ${FILENAME} -f best)
+					omxplayer -o hdmi -r $VURL --live > /dev/null 2>>/dev/null
+					sleep $VIDEO_RECOVER
+				else
+					sleep 0	
 				fi
+				# wait to finish running processes and move on
+				while ps ax | grep -v grep | grep -E "omxplayer|fbi" > /dev/null; do
+					sleep 0.1
+				done;
+
 			else
 				sleep 1		
 			fi
+		} < /dev/null; 
 		done;
 	else 
 		sleep 1
-	fi
-done;
+	fi	
+	wait $!	
+done
