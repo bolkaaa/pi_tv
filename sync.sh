cd198217c1364092918909f04c7b4ea43c3b3bce
bolka <kobolka.matus@gmail.com>
2018-08-01 16:02:02 +0200

    rebooting

--- sync.sh
+++ sync.sh
@@ -7,6 +7,13 @@ if [ -e .uri ]; then
   ACCOUNT_ID=$(echo $ACCOUNT_PATH | awk -F "media/" '{print $2}')	
   LOG_FILE=$(echo $ACCOUNT_PATH | awk -F "/static" '{print $1}')/logs/${ACCOUNT_ID}.log
   SSH_ARGS="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+  # check for reboot request
+  rsync --remove-source-files -avz -e "${SSH_ARGS}" ${REMOTE_URI}/tmp/.reboot ${HOME_FOLDER}/.reboot
+  if [ -e ${HOME_FOLDER}/.reboot ]; then
+  rm ${HOME_FOLDER}/.reboot
+  reboot	
+  fi	
+  #	
   rsync -avz -L -e "${SSH_ARGS}" ${REMOTE_URI}/uploads/ ${HOME_FOLDER}/media/
   rsync -avz -e "${SSH_ARGS}" --rsync-path="rm -f ${LOG_FILE} && rsync --log-file=${LOG_FILE}" ${REMOTE_URI}/playlist ${HOME_FOLDER}/playlist
 fi
